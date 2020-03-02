38035fb352a32263e2338528a382eab5039c6952
J1040083 <huang.yahui.grace@gmail.com>
2020-03-02 17:27:27 +0800

    新增元件查詢.

--- src/main/java/com/myyhhuang/web/HelloController.java
+++ src/main/java/com/myyhhuang/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.myyhhuang.web;
 
 import com.myyhhuang.beanim.IMCounty;
+import com.myyhhuang.test.User;
 import com.myyhhuang.transactional.IMServiceEntry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,9 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -74,4 +73,18 @@ public class HelloController {
         return imCountyList;
     }
 
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
+        return new User(name, age);
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String hello(@RequestBody User user) {
+        return "Hello " + user.getName() + ", " + user.getAge();
+    }
 }
